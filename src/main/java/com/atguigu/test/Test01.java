62d320d61ae45fe489cd0421294daa72ab5fb0ac
ppdy <ppdy@163.com>
2022-06-20 09:43:16 +0800

    third commit

--- src/main/java/com/atguigu/test/Test01.java
+++ src/main/java/com/atguigu/test/Test01.java
@@ -4,6 +4,7 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("888");
         System.out.println("3333");
+        System.out.println("234");
     }
 
 }
